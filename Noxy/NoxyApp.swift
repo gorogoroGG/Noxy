781dfa1b91809104984dc097faf3d590d23f6738
gorogoroGG <watch.yugo@gmail.com>
2026-05-27 18:23:16 +0900

    Initial Commit

---
+++ Noxy/NoxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoxyApp.swift
+//  Noxy
+//
+//  Created by ゴロゴロ on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct NoxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
